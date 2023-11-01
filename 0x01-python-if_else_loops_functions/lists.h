7c90eb931be91e5b911e69ddef3a731af39f904a
moh <mohamedmld954@gmail.com>
2023-11-01 14:08:58 -0700

    fdfd

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -8,7 +8,7 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- * sfm,fd,
+ * sdssfm,fd,
  * Description: singly linked list node structure
  */
 typedef struct listint_s
