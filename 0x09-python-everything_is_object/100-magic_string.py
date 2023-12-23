d1ca7a16c718dc8ac7c7980e5b6a3f8604f7240a
mohamedNumberOne <79853513+mohamedNumberOne@users.noreply.github.com>
2023-12-23 22:52:12 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(List_of_str=[]):
-    List_of_str += ["BestSchool"]
-    return (", ".join(List_of_str))
--- 0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(lo):
-    return lo.copy()
